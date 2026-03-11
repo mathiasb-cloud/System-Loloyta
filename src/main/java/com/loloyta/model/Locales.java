b2e536bf1b81c8c9cb654a289aa32fc32821966f
diego <dtiradot@gmail.com>
2026-03-10 23:30:35 -0500

    locales

---
+++ src/main/java/com/loloyta/model/Locales.java
@@ -0,0 +1,15 @@
+package com.loloyta.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+public class Locales {
+
+}
