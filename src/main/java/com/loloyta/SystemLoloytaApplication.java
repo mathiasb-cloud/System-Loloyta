69aa6dbb06b2cf324c4c92a37098a6efa3e7cc90
Mathias Benites <i202406644@cibertec.edu.pe>
2026-03-10 22:43:58 -0500

    proyecto base

---
+++ src/main/java/com/loloyta/SystemLoloytaApplication.java
@@ -0,0 +1,13 @@
+package com.loloyta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemLoloytaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemLoloytaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loloyta/SystemLoloytaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loloyta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemLoloytaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
