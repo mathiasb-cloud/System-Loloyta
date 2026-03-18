94b0548872d2cf28ad9a29a7aa48cc30ce6d8488
Mathias Benites <benitesmathiaa@gmail.com>
2026-03-17 22:44:10 -0500

    repository

--- src/main/java/com/loloyta/repository/StockRepository.java
+++ src/main/java/com/loloyta/repository/StockRepository.java
@@ -1,8 +1,12 @@
 package com.loloyta.repository;
 
-import com.loloyta.model.Stock;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.loloyta.model.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    Optional<Stock> findByAlmacenesIdAndProductoId(Long almacenId, Long productoId);
 
 }
